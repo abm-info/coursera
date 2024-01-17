44111d128b12246e395e893c87e569e1ce64606e
abm-info <113168574+abm-info@users.noreply.github.com>
2024-01-17 13:14:46 +0100

    Update autos.php

--- autos.php
+++ autos.php
@@ -1,38 +1,30 @@
 <?php
-
 require_once "pdo.php";
 $failure = false;
 $success = false;
 
-if (!isset($_GET['name']))
-{
+if (!isset($_GET['name'])) {
     die("Name parameter missing");
-}
-elseif (isset($_POST['logout']) && $_POST['logout'] == 'Logout')
-{
+} elseif (isset($_POST['logout']) && $_POST['logout'] == 'Logout') {
     header('Location: index.php');
 }
-elseif (isset($_POST['make']) && isset($_POST['year'])
-    && isset($_POST['mileage'])) {
-    if (!is_numeric($_POST['year']) || !is_numeric($_POST['mileage']))
-    {
+
+if (isset($_POST['make']) && isset($_POST['year']) && isset($_POST['mileage'])) {
+    if (!is_numeric($_POST['year']) || !is_numeric($_POST['mileage'])) {
         $failure = 'Mileage and year must be numeric';
-    }
-    elseif (strlen($_POST['make']) < 1 )
-    {
+    } elseif (strlen($_POST['make']) < 1) {
         $failure = 'Make is required';
-    }
-    else
-    {
+    } else {
         $stmt = $pdo->prepare('INSERT INTO autos (make, year, mileage) VALUES ( :make, :year, :mileage)');
         $stmt->execute(array(
-                ':make' => $_POST['make'],
-                ':year' => $_POST['year'],
-                ':mileage' => $_POST['mileage'])
-        );
+            ':make' => $_POST['make'],
+            ':year' => $_POST['year'],
+            ':mileage' => $_POST['mileage']
+        ));
         $success = 'Record inserted';
     }
 }
+
 $stmt = $pdo->query("SELECT make, year, mileage FROM autos");
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -47,18 +39,14 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <div class="container">
     <h1>Tracking Autos for <?php echo $_GET['name']; ?></h1>
     <?php
-    if ($failure !== false)
-    {
-        // Look closely at the use of single and double quotes
+    if ($failure !== false) {
         echo('<p style="color: red;">' . htmlentities($failure) . "</p>\n");
     }
-    if ($success !== true)
-    {
-        // Look closely at the use of single and double quotes
+    if ($success !== false) {
         echo('<p style="color: green;">' . htmlentities($success) . "</p>\n");
     }
     ?>
-    <form method="post">
+    <form method="post" action="autos.php">
         <p>Make:
             <input type="text" name="make" size="60"/></p>
         <p>Year:
@@ -71,15 +59,14 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     <h2>Automobiles</h2>
     <ul>
-
         <?php
-        foreach ($rows as $row)
-        {
+        foreach ($rows as $row) {
             echo '<li>';
             echo htmlentities($row['make']) . ' ' . $row['year'] . ' / ' . $row['mileage'];
-        };
-        echo '</li><br/>';
+            echo '</li>';
+        }
         ?>
     </ul>
 </div>
 </body>
+</html>
